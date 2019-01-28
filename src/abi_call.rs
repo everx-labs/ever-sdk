94ab69a5132c6a6d8a8a46f616d27ab8b67d891f
Alexey Vavilin <mnogoborec@mail.ru>
2019-01-28 23:48:08 +0300

    Add tests

--- src/abi_call.rs
+++ src/abi_call.rs
@@ -7,7 +7,7 @@ use tonlabs_sdk_emulator::cells_serialization::BagOfCells;
 use tonlabs_sdk_emulator::bitstring::Bitstring;
 use crate::abi_parameter::{ABIParameter, append_data};
 
-const ABI_VERSION: u8 = 0;
+pub const ABI_VERSION: u8 = 0;
 
 pub struct ABICall<TIn: ABIParameter, TOut: ABIParameter> {
     input: PhantomData<TIn>,
