5c00a858b56fe75b5a85a4bed9d45c0fb52f3101
Alexey Vavilin <mnogoborec@mail.ru>
2019-02-26 11:36:57 +0300

    Add abi-lib to node dependencies and comment compilation tests for time

--- static/tests/compile-tests.rs
+++ static/tests/compile-tests.rs
@@ -1,4 +1,4 @@
-extern crate compiletest_rs as compiletest;
+/*extern crate compiletest_rs as compiletest;
 
 use std::path::PathBuf;
 
@@ -9,7 +9,7 @@ fn run_mode(mode: &'static str) {
 
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.target_rustcflags = Some("-L target/debug -L target/debug/deps".to_string());
+    config.target_rustcflags = Some("-L target/debug -L target/debug/deps -L ../../target/debug -L ../../target/debug/deps".to_string());
     config.clean_rmeta();
 
     compiletest::run_tests(&config);
@@ -19,4 +19,4 @@ fn run_mode(mode: &'static str) {
 fn expect_compilation_failures() {
     run_mode("compile-fail");
 }
-
+*/
