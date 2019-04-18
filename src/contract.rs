41349f366b69bd209616bea56ccc94c5847f2b77
Kirill Zavarovskiy <zavarovskii@ya.ru>
2019-04-18 16:06:02 +0300

    Added some comments

--- src/contract.rs
+++ src/contract.rs
@@ -32,15 +32,39 @@ pub struct Contract {
 }
 
 impl Contract {
-    fn load(id: AccountId) -> SdkResult<NodeResponce<Contract>> {
+    pub fn load(id: AccountId) -> SdkResult<NodeResponce<Contract>> {
         unimplemented!()
     }
 
-    fn deploy(image: ContractImage) -> SdkResult<ChangesStream<ContractCallState>> {
+    pub fn deploy(image: ContractImage) -> SdkResult<ChangesStream<ContractCallState>> {
         unimplemented!()
+
+        // Deploy is call, but special message is constructed.
+        // The message contains StateInit struct with code, public key and lib
+        // and body with parameters for contract special method - constructor.
+
     }
 
-    fn call() -> SdkResult<ChangesStream<ContractCallState>> {
+    pub fn call() -> SdkResult<ChangesStream<ContractCallState>> {
+        unimplemented!()
+
+        // pack params into bag of cells via ABI
+        // message_id = message's hash
+
+        // send message by Kafka
+        // (synchroniously - when responce will returned message will be in DB)
+
+        // subscribe on updates from DB and return updates stream
+
+    }
+
+    fn send_message() -> SdkResult<()> {
+        unimplemented!()
+
+        // send message by Kafka
+    }
+
+    fn subscribe_updates() -> SdkResult<ChangesStream<ContractCallState>> {
         unimplemented!()
     }
 }
