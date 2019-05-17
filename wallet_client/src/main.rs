5c80d23df26345ba3fca3a7675e8965296a358bb
Alexey Vavilin <mnogoborec@mail.ru>
2019-05-17 17:08:00 +0300

    sendTransaction

--- wallet_client/src/main.rs
+++ wallet_client/src/main.rs
@@ -1,5 +1,9 @@
 extern crate ton_sdk;
 extern crate hex;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 use rand::{thread_rng, Rng};
 use ton_block::{Message, MsgAddressExt, MsgAddressInt, InternalMessageHeader, Grams, 
@@ -9,6 +13,8 @@ use tvm::types::AccountId;
 use ed25519_dalek::Keypair;
 use futures::Stream;
 use sha2::Sha512;
+
+use abi_lib_dynamic::json_abi::decode_function_responce;
 
 use ton_sdk::*;
 
@@ -255,14 +261,14 @@ pub fn create_external_transfer_funds_message(src: AccountId, dst: AccountId, va
 
 fn deploy_contract_and_wait(code_file_name: &str, abi: &str, constructor_params: &str, key_pair: &Keypair) -> AccountId {
     // read image from file and construct ContractImage
-    let mut state_init = std::fs::File::open("src/tests/".to_owned() + code_file_name).expect("Unable to open contract code file");
+    let mut state_init = std::fs::File::open(code_file_name).expect("Unable to open contract code file");
 
     let contract_image = ContractImage::from_state_init_and_key(&mut state_init, &key_pair).expect("Unable to parse contract code file");
 
     let account_id = contract_image.account_id();
 
     // before deploying contract need to transfer some funds to its address
-    println!("Account ID to take some grams {}\n", account_id);
+    //println!("Account ID to take some grams {}\n", account_id);
     let msg = create_external_transfer_funds_message(AccountId::from([0_u8; 32]), account_id.clone(), 100);
     Contract::send_message(msg).unwrap();
 
@@ -281,7 +287,7 @@ fn deploy_contract_and_wait(code_file_name: &str, abi: &str, constructor_params:
             panic!("error next state getting: {}", e);
         }
         if let Ok(s) = state {
-            println!("next state: {:?}", s);
+            //println!("next state: {:?}", s);
             if s.message_state == MessageState::Finalized {
                 tr_id = Some(s.message_id.clone());
                 break;
@@ -295,13 +301,84 @@ fn deploy_contract_and_wait(code_file_name: &str, abi: &str, constructor_params:
 	account_id
 }
 
-fn call_create(params: &[&str]) {
+
+fn call_contract_and_wait(address: AccountId, func: &str, input: &str, abi: &str, key_pair: &Keypair) -> String {
+
+    let contract = Contract::load(address)
+        .expect("Error calling load Contract")
+        .wait()
+        .next()
+        .expect("Error unwrap stream next while loading Contract")
+        .expect("Error unwrap result while loading Contract");
+
+    // call needed method
+    let changes_stream = contract.call_json(func.to_owned(), input.to_owned(), abi.to_owned(), Some(&key_pair))
+        .expect("Error calling contract method");
+
+    // wait transaction id in message-status 
+    let mut tr_id = None;
+    for state in changes_stream.wait() {
+        if let Err(e) = state {
+            panic!("error next state getting: {}", e);
+        }
+        if let Ok(s) = state {
+            //println!("next state: {:?}", s);
+            if s.message_state == MessageState::Finalized {
+                tr_id = Some(s.message_id.clone());
+                break;
+            }
+        }
+    }
+    let tr_id = tr_id.expect("Error: no transaction id");
+
+    // OR 
+    // wait message will done and find transaction with the message
+
+    // load transaction object
+    let tr = Transaction::load(tr_id)
+        .expect("Error calling load Transaction")
+        .wait()
+        .next()
+        .expect("Error unwrap stream next while loading Transaction")
+        .expect("Error unwrap result while loading Transaction");
+
+    // take external outbound message from the transaction
+    let out_msg = tr.load_out_messages()
+        .expect("Error calling load out messages")
+        .wait()
+        .find(|msg| msg.as_ref().expect("erro unwrap out message").msg_type() == MessageType::ExternalOutbound)
+            .expect("erro unwrap out message 2")
+            .expect("erro unwrap out message 3");
+
+    // take body from the message
+    let responce = out_msg.body().into();
+
+    // decode the body by ABI
+    let result = decode_function_responce(abi.to_owned(), func.to_owned(), responce)
+        .expect("Error decoding result");
+
+    println!("Contract call result: {}\n", result);
+
+	result
+
+    // this way it is need:
+    // 1. message status with transaction id or transaction object with in-message id
+    // 2. transaction object with out messages ids
+    // 3. message object with body
+}
+
+fn call_create() {
+	println!("Creating new wallet account");
+
     // generate key pair
     let mut csprng = rand::rngs::OsRng::new().unwrap();
     let keypair = Keypair::generate::<Sha512, _>(&mut csprng);
    
 	// deploy wallet
     let wallet_address = deploy_contract_and_wait("Wallet.tvc", WALLET_ABI, "{}", &keypair);
+
+    println!("Acoount created. Address {}", hex::encode(wallet_address.as_slice()));
+
 
     unsafe {
         current_address = Some(wallet_address);
@@ -330,6 +407,55 @@ fn call_get_balance(params: &[&str]) {
 	println!("Account balance {}", balance);
 }
 
+#[derive(Deserialize)]
+struct SendTransactionAnswer {
+	transaction: u64,
+	error: i8
+}
+
+fn call_send_transaction(params: &[&str]) {
+    if params.len() < 2 {
+        println!("Not enough parameters");
+        return;
+    }
+
+	unsafe {
+	    if None == current_address {
+	    	println!("Current address not set");
+	    	return;
+	    }
+	}
+
+    let str_params = format!("{{ \"recipient\" : \"x{}\", \"value\": \"{}\" }}", params[0], params[1]);
+
+    let mut answer: String;
+
+    unsafe {
+    	//println!("Address {}", current_address.clone().unwrap());
+
+    	let pair = if let Some(pair) = &current_key_pair {
+    		let array = pair.to_bytes();
+
+    		Keypair::from_bytes(&array).unwrap()
+    	} else {
+    		panic!("Current key pair not set");
+    	};
+
+    	answer = call_contract_and_wait(current_address.clone().unwrap(), "sendTransaction", &str_params, WALLET_ABI, &pair);
+    }
+
+    let answer: SendTransactionAnswer = serde_json::from_str(&answer).unwrap();
+
+    println!("Transaction ID {}", answer.transaction);
+}
+
+const HELP: &str = r#"
+Supported commands:
+    balance <address>       - get the account balance. If address is not provided current address is used
+    create                  - create new wallet account and set as current
+    send <address> <value>  - send <value> grams to <address>
+    exit                    - exit program"#;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -341,15 +467,24 @@ fn main() {
 
     init_json(config).expect("Couldn't establish connection");
 
+    println!("Connection established");
+    println!("Enter command");
+
     loop {
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).expect("error: unable to read user input");
 
         let params: Vec<&str> = input.split_whitespace().collect();
 
+        if params.len() == 0 {
+        	continue;
+        }
+
         match params[0].as_ref() {
+        	"help" => println!("{}", HELP),
 			"balance" => call_get_balance(&params[1..]),
-            "create" => call_create(&params[1..]),
+            "create" => call_create(),
+            "send" => call_send_transaction(&params[1..]),
             "exit" => break,
             _ => println!("Unknown command")
         }
