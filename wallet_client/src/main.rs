05c7023c32108965868a4d74f79c38b014922e82
Alexey Vavilin <mnogoborec@mail.ru>
2019-05-17 14:39:33 +0300

    First try

---
+++ wallet_client/src/main.rs
@@ -0,0 +1,357 @@
+extern crate ton_sdk;
+extern crate hex;
+
+use rand::{thread_rng, Rng};
+use ton_block::{Message, MsgAddressExt, MsgAddressInt, InternalMessageHeader, Grams, 
+    ExternalInboundMessageHeader, CurrencyCollection, Serializable};
+use tvm::bitstring::Bitstring;
+use tvm::types::AccountId;
+use ed25519_dalek::Keypair;
+use futures::Stream;
+use sha2::Sha512;
+
+use ton_sdk::*;
+
+const STD_CONFIG: &str = r#"
+{
+    "db_config": {
+        "servers": ["142.93.137.28:28015"],
+        "db_name": "blockchain"
+    },
+    "kafka_config": {
+        "servers": ["142.93.137.28:9092"],
+        "topic": "requests-1",
+        "ack_timeout": 1000
+    }
+}"#;
+
+
+const WALLET_ABI: &str = r#"{
+	"ABI version" : 0,
+
+	"functions" :	[
+	    {
+	        "inputs": [
+	            {
+	                "name": "recipient",
+	                "type": "bits256"
+	            },
+	            {
+	                "name": "value",
+	                "type": "duint"
+	            }
+	        ],
+	        "name": "sendTransaction",
+					"signed": true,
+	        "outputs": [
+	            {
+	                "name": "transaction",
+	                "type": "uint64"
+	            },
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+	    {
+	        "inputs": [
+						  {
+	                "name": "type",
+	                "type": "uint8"
+	            },
+							{
+	                "name": "value",
+	                "type": "duint"
+	            },
+							{
+	                "name": "meta",
+	                "type": "bitstring"
+	            }
+					],
+	        "name": "createLimit",
+					"signed": true,
+	        "outputs": [
+							{
+	                "name": "limitId",
+	                "type": "uint8"
+	            },
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+	    {
+	        "inputs": [
+							{
+	                "name": "limitId",
+	                "type": "uint8"
+	            },
+							{
+	                "name": "value",
+	                "type": "duint"
+	            },
+							{
+	                "name": "meta",
+	                "type": "bitstring"
+	            }
+	        ],
+	        "name": "changeLimitById",
+					"signed": true,
+	        "outputs": [
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [
+							{
+	                "name": "limitId",
+	                "type": "uint8"
+	            }
+	        ],
+	        "name": "removeLimit",
+					"signed": true,
+	        "outputs": [
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [
+							{
+	                "name": "limitId",
+	                "type": "uint8"
+	            }
+	        ],
+	        "name": "getLimitById",
+	        "outputs": [
+							{
+									"name": "limitInfo",
+					        "type": "tuple",
+					        "components": [
+											{
+					                "name": "value",
+					                "type": "duint"
+					            },
+											{
+					                "name": "type",
+					                "type": "uint8"
+					            },
+											{
+					                "name": "meta",
+					                "type": "bitstring"
+					            }
+									]
+							},
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [],
+	        "name": "getLimits",
+	        "outputs": [
+							{
+									"name": "list",
+					        "type": "uint8[]"
+							},
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [],
+	        "name": "getVersion",
+	        "outputs": [
+							{
+									"name": "version",
+					        "type": "tuple",
+					        "components": [
+											{
+					                "name": "major",
+					                "type": "uint16"
+					            },
+											{
+					                "name": "minor",
+					                "type": "uint16"
+					            }
+									]
+							},
+							{
+	                "name": "error",
+	                "type": "int8"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [],
+	        "name": "getBalance",
+	        "outputs": [
+							{
+	                "name": "balance",
+	                "type": "uint64"
+	            }
+	        ]
+	    },
+			{
+	        "inputs": [],
+	        "name": "constructor",
+	        "outputs": []							
+	    },
+			{
+	        "inputs": [{"name": "address", "type": "bits256" }],
+	        "name": "setSubscriptionAccount",
+					"signed": true,
+	        "outputs": []							
+	    },
+			{
+	        "inputs": [],
+	        "name": "getSubscriptionAccount",
+	        "outputs": [{"name": "address", "type": "bits256" }]							
+	    }
+	]
+}
+"#;
+
+static mut current_key_pair: Option<Keypair> = None;
+static mut current_address: Option<AccountId> = None;
+
+
+// Create message "from wallet" to transfer some funds 
+// from one account to another
+pub fn create_external_transfer_funds_message(src: AccountId, dst: AccountId, value: u128) -> Message {
+    
+    let mut rng = thread_rng();    
+    let mut msg = Message::with_ext_in_header(
+        ExternalInboundMessageHeader {
+            src: MsgAddressExt::with_extern(&Bitstring::from(rng.gen::<u64>())).unwrap(),
+            dst: MsgAddressInt::with_standart(None, 0, src.clone()).unwrap(),
+            import_fee: Grams::default(),
+        }
+    );
+
+    let mut balance = CurrencyCollection::default();
+    balance.grams = Grams(value.into());
+
+    let int_msg_hdr = InternalMessageHeader::with_addresses(
+            MsgAddressInt::with_standart(None, 0, src).unwrap(),
+            MsgAddressInt::with_standart(None, 0, dst).unwrap(),
+            balance);
+
+    msg.body = Some(int_msg_hdr.write_to_new_cell().unwrap().into());
+
+    msg
+}
+
+fn deploy_contract_and_wait(code_file_name: &str, abi: &str, constructor_params: &str, key_pair: &Keypair) -> AccountId {
+    // read image from file and construct ContractImage
+    let mut state_init = std::fs::File::open("src/tests/".to_owned() + code_file_name).expect("Unable to open contract code file");
+
+    let contract_image = ContractImage::from_state_init_and_key(&mut state_init, &key_pair).expect("Unable to parse contract code file");
+
+    let account_id = contract_image.account_id();
+
+    // before deploying contract need to transfer some funds to its address
+    println!("Account ID to take some grams {}\n", account_id);
+    let msg = create_external_transfer_funds_message(AccountId::from([0_u8; 32]), account_id.clone(), 100);
+    Contract::send_message(msg).unwrap();
+
+
+    // call deploy method
+    let changes_stream = Contract::deploy_json("constructor".to_owned(), constructor_params.to_owned(), abi.to_owned(), contract_image, Some(key_pair))
+        .expect("Error deploying contract");
+
+    // wait transaction id in message-status or 
+    // wait message will done and find transaction with the message
+
+    // wait transaction id in message-status 
+    let mut tr_id = None;
+    for state in changes_stream.wait() {
+        if let Err(e) = state {
+            panic!("error next state getting: {}", e);
+        }
+        if let Ok(s) = state {
+            println!("next state: {:?}", s);
+            if s.message_state == MessageState::Finalized {
+                tr_id = Some(s.message_id.clone());
+                break;
+            }
+        }
+    }
+    // contract constructor doesn't return any values so there are no output messages in transaction
+    // so just check deployment transaction created
+    let _tr_id = tr_id.expect("Error: no transaction id");
+
+	account_id
+}
+
+fn call_create(params: &[&str]) {
+    // generate key pair
+    let mut csprng = rand::rngs::OsRng::new().unwrap();
+    let keypair = Keypair::generate::<Sha512, _>(&mut csprng);
+   
+	// deploy wallet
+    let wallet_address = deploy_contract_and_wait("Wallet.tvc", WALLET_ABI, "{}", &keypair);
+
+    unsafe {
+        current_address = Some(wallet_address);
+        current_key_pair = Some(keypair);
+    }
+}
+
+fn call_get_balance(params: &[&str]) {
+		let address = if params.len() > 0 {
+			AccountId::from(hex::decode(params[0]).unwrap())
+		} else {
+			unsafe {
+				current_address.clone().unwrap()
+			}
+		};
+
+	let contract = Contract::load(address)
+        .expect("Error calling load Contract")
+        .wait()
+        .next()
+        .expect("Error unwrap stream next while loading Contract")
+        .expect("Error unwrap result while loading Contract");
+
+	let balance = contract.balance_grams();
+
+	println!("Account balance {}", balance);
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let config = if args.len() > 3 && args[2] == "-config" {
+        std::fs::read_to_string(&args[3]).expect("Couldn't read config file")
+    } else {
+        STD_CONFIG.to_owned()
+    };
+
+    init_json(config).expect("Couldn't establish connection");
+
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("error: unable to read user input");
+
+        let params: Vec<&str> = input.split_whitespace().collect();
+
+        match params[0].as_ref() {
+			"balance" => call_get_balance(&params[1..]),
+            "create" => call_create(&params[1..]),
+            "exit" => break,
+            _ => println!("Unknown command")
+        }
+    }
+}
