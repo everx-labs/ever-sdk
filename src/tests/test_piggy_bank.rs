d0ef3280826bd61165001725b9aa8e046f46ac79
Alexey Vavilin <mnogoborec@mail.ru>
2019-05-27 20:35:10 +0300

    responce -> response

--- src/tests/test_piggy_bank.rs
+++ src/tests/test_piggy_bank.rs
@@ -1,4 +1,4 @@
-use abi_lib_dynamic::json_abi::decode_function_responce;
+use abi_lib_dynamic::json_abi::decode_function_response;
 use super::*;
 use ed25519_dalek::Keypair;
 use rand::rngs::OsRng;
@@ -375,7 +375,7 @@ fn call_contract_and_wait(address: AccountId, func: &str, input: &str, abi: &str
 
 
     // decode the body by ABI
-    let result = decode_function_responce(abi.to_owned(), func.to_owned(), response)
+    let result = decode_function_response(abi.to_owned(), func.to_owned(), response)
         .expect("Error decoding result");
 
     println!("Contract call result: {}\n", result);
