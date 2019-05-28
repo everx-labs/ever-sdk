c3765b49df54fa70c940269b6ecf2978bd9225b0
Kirill Zavarovskiy <zavarovskii@ya.ru>
2019-05-28 17:27:53 +0300

    fixed config for full_test_piggy_bank

--- src/tests/test_piggy_bank.rs
+++ src/tests/test_piggy_bank.rs
@@ -398,7 +398,7 @@ fn init_node_connection() {
             },
             "kafka_config": {
                 "servers": ["142.93.137.28:9092"],
-                "topic": "requests-ilya",
+                "topic": "requests",
                 "ack_timeout": 1000
             }
         }"#;    
