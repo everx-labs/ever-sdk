f699daf835bb8bd0a0c82276239e902b14c86255
Alexey Vavilin <mnogoborec@mail.ru>
2019-08-08 09:59:28 +0300

    Revert "Contracts updated"
    This reverts commit f580ef0be3a73c140006dc38e5fd1eb1559bfde8.

--- ton_sdk/src/tests/test_piggy_bank.rs
+++ ton_sdk/src/tests/test_piggy_bank.rs
@@ -295,7 +295,7 @@ fn init_node_connection() {
             },
             "kafka_config": {
                 "servers": ["142.93.137.28:9092"],
-                "topic": "requests-alexey",
+                "topic": "requests",
                 "ack_timeout": 1000
             }
         }"#;    
