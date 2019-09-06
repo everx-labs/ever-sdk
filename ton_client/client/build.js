cdfbbe80e5f3e1d46ecb25b711af4c5470137fc0
AtomXY <alexvd@inbox.ru>
2019-09-06 18:13:18 +0300

    Fix path in build.js

--- ton_client/client/build.js
+++ ton_client/client/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const os = require('os');
 const {gz, spawnProcess, deleteFolderRecursive, main, version, root_path} = require('../platforms/build-lib');
-
+const platform = os.platform();
 
 main(async() => {
     // await spawnProcess('cargo', ['clean']);
@@ -9,16 +9,16 @@ main(async() => {
     await spawnProcess('cargo', ['build', '--release']);
     deleteFolderRecursive(root_path('bin'));
     fs.mkdirSync(root_path('bin'), { recursive: true });
-    switch(os.platform()) {
+    switch(platform) {
         case 'linux':
-            await gz(['target', 'release', 'libton_client.dylib'], `tonclient_${version}_${platform}`);
+            await gz(['..', '..', 'target', 'release', 'libton_client.so'], `tonclient_${version}_${platform}`);
             break;
         case 'win32':
-            await gz(['target', 'release', 'ton_client.dll.lib'], `tonclient_${version}_${platform}.lib`);
-            await gz(['target', 'release', 'ton_client.dll'], `tonclient_${version}_${platform}.dll`);
+            await gz(['..', '..', 'target', 'release', 'ton_client.dll.lib'], `tonclient_${version}_${platform}.lib`);
+            await gz(['..', '..', 'target', 'release', 'ton_client.dll'], `tonclient_${version}_${platform}.dll`);
             break;
         case 'darwin':
-            await gz(['target', 'release', 'libton_client.dylib'], `tonclient_${version}_${platform}`);
+            await gz(['..', '..', 'target', 'release', 'libton_client.dylib'], `tonclient_${version}_${platform}`);
             break;
         default:
     }
