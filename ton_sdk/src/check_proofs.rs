02e6cc4b25a79f6e3cfeb7e31f3cf63740fafe01
Kirill Zavarovskiy <zavarovskii@ya.ru>
2019-10-15 18:29:12 +0300

    Done check_account function

--- ton_sdk/src/check_proofs.rs
+++ ton_sdk/src/check_proofs.rs
@@ -2,7 +2,7 @@ use crate::*;
 use std::str::FromStr;
 use serde_json::Value;
 use tvm::block::{ Transaction as TvmTransaction, Message as TvmMessage, 
-    TransactionProcessingStatus, Deserializable, Account as TvmAccount, check_account_proof,
+    TransactionProcessingStatus, Deserializable, Account as TvmAccount, AccountStuff, check_account_proof,
     check_transaction_proof, check_message_proof, MerkleProof, MessageProcessingStatus };
 use tvm::cells_serialization::deserialize_tree_of_cells;
 use tvm::types::UInt256;
@@ -191,7 +191,7 @@ pub fn check_message(msg_val: &Value) -> SdkResult<()> {
 
 
 /// Checks merkle proof of account. Takes serde_json::Value with account struct.
-/// Value must contain `boc` and `proof` fields.
+/// Value must contain `addr.AddrStd.address`, `boc` and `proof` fields.
 /// The next checks are performed:
 /// * check if all account's fields from value 
 /// is corresponds to values in account constructed from boc;
@@ -234,7 +234,9 @@ pub fn check_account(acc_val: &Value) -> SdkResult<UInt256> {
 
 
     // build full account from BOC
-    let mut full_acc: TvmAccount = TvmAccount::construct_from(&mut boc.into())?;
+    // We don't work with Account::AccountNone, so boc contains serialized stuff 
+    // of Account::Account it is AccountStuff struct
+    let full_acc: TvmAccount = TvmAccount::Account(AccountStuff::construct_from(&mut boc.into())?);
 
     // and proof
     let proof: MerkleProof = MerkleProof::construct_from(&mut proof.into())?;
