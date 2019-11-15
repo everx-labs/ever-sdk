df9a76e6e54958231e985280300d5056aeff11ca
Victor Korsakov <26911098+diserere@users.noreply.github.com>
2019-11-15 06:06:16 +0300

    Add lic to every .rs file

--- ton_client/client/src/tests/mod.rs
+++ ton_client/client/src/tests/mod.rs
@@ -1,3 +1,17 @@
+/*
+* Copyright 2018-2019 TON DEV SOLUTIONS LTD.
+*
+* Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+* this file except in compliance with the License.  You may obtain a copy of the
+* License at: https://ton.dev/licenses
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific TON DEV software governing permissions and
+* limitations under the License.
+*/
+
 use crypto::keys::account_decode;
 use ::{InteropContext, JsonResponse};
 use ::{tc_json_request, InteropString};
@@ -53,7 +67,7 @@ fn test_wallet_deploy() {
         println!("result: {}", version.result_json.to_string());
 
         let _deployed = json_request(context, "setup",
-            json!({"baseUrl": "http://192.168.99.100"}));
+            json!({"baseUrl": "http://0.0.0.0"}));
 
 		let keys = json_request(context, "crypto.ed25519.keypair", json!({}));
 
--- ton_sdk/src/tests/tests_common.rs
+++ ton_sdk/src/tests/tests_common.rs
@@ -1,3 +1,17 @@
+/*
+* Copyright 2018-2019 TON DEV SOLUTIONS LTD.
+*
+* Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+* this file except in compliance with the License.  You may obtain a copy of the
+* License at: https://ton.dev/licenses
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific TON DEV software governing permissions and
+* limitations under the License.
+*/
+
 use super::*;
 use ed25519_dalek::{Keypair, SecretKey, PublicKey};
 use futures::Stream;
@@ -60,11 +74,11 @@ pub fn init_node_connection() {
         r#"
         {
             "queries_config": {
-                "queries_server": "http://192.168.99.100/graphql",
-                "subscriptions_server": "ws://192.168.99.100/graphql"
+                "queries_server": "http://0.0.0.0/graphql",
+                "subscriptions_server": "ws://0.0.0.0/graphql"
             },
             "requests_config": {
-                "requests_server": "http://192.168.99.100/topics/requests"
+                "requests_server": "http://0.0.0.0/topics/requests"
             }
         }"#
     } else {
