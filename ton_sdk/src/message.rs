1b07bf1af74d830e02f556f6773d1acad77d7be1
Eugenia Makarova <makarova.evgenia@gmail.com>
2020-01-09 12:55:04 +0300

    change 2019 to 2020

--- ton_sdk/src/message.rs
+++ ton_sdk/src/message.rs
@@ -1,5 +1,5 @@
 /*
-* Copyright 2018-2019 TON DEV SOLUTIONS LTD.
+* Copyright 2018-2020 TON DEV SOLUTIONS LTD.
 *
 * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
 * this file except in compliance with the License.  You may obtain a copy of the
