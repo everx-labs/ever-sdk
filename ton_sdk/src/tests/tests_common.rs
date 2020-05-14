5124e6d35e22659169bd18472abbd43ce2425ffd
Alexey Vavilin <mnogoborec@mail.ru>
2020-05-14 09:18:40 +0300

    Update rand for wasm

--- ton_sdk/src/tests/tests_common.rs
+++ ton_sdk/src/tests/tests_common.rs
@@ -17,7 +17,7 @@ use std::str::FromStr;
 use ton_block::MsgAddressInt;
 use futures::StreamExt;
 
-const NODE_SE: bool = false;
+const NODE_SE: bool = true;
 
 const GIVER_ADDRESS_STR:  &str = "0:841288ed3b55d9cdafa806807f02a0ae0c169aa5edfe88a789a6482429756a94";
 
