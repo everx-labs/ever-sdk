fc9202b5cb5717d9bd3273fe923321ecb400e8a0
Michael Vlasov <melsomino@gmail.com>
2020-07-31 15:33:27 +0500

    FIX: client build libname

--- ton_client/client/build.js
+++ ton_client/client/build.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const os = require('os');
-const path = require('os');
-const { gz, spawnProcess, deleteFolderRecursive, main, version, root_path } = require('../platforms/build-lib');
+const {
+    gz,
+    spawnProcess,
+    deleteFolderRecursive,
+    main,
+    version,
+    root_path,
+} = require('../platforms/build-lib');
 const platform = os.platform();
-
 
 main(async () => {
     // await spawnProcess('cargo', ['clean']);
@@ -11,33 +16,14 @@ main(async () => {
     await spawnProcess('cargo', ['build', '--release']);
     deleteFolderRecursive(root_path('bin'));
     fs.mkdirSync(root_path('bin'), { recursive: true });
-    switch (platform) {
-    case 'linux':
+    const platformNames = {
+        linux: [['lib{}.so', '']],
+        win32: [['{}.dll.lib', '_lib'], ['{}.dll', '_dll']],
+        darwin: [['lib{}.dylib', '']],
+    };
+    for (const [src, dstSuffix] of platformNames[platform] || []) {
         await gz(
-            ['..', '..', 'target', 'release', 'libtonclient.so'],
-            `tonclient_${version}_${platform}`,
-            [__dirname, 'build'],
-        );
-        break;
-    case 'win32':
-        await gz(
-            ['..', '..', 'target', 'release', 'tonclient.dll.lib'],
-            `tonclient_${version}_${platform}_lib`,
-            [__dirname, 'build'],
-        );
-        await gz(
-            ['..', '..', 'target', 'release', 'tonclient.dll'],
-            `tonclient_${version}_${platform}_dll`,
-            [__dirname, 'build'],
-        );
-        break;
-    case 'darwin':
-        await gz(
-            ['..', '..', 'target', 'release', 'libtonclient.dylib'],
-            `tonclient_${version}_${platform}`,
-            [__dirname, 'build'],
-        );
-        break;
-    default:
+            ['..', '..', 'target', 'release', src.replace('{}', 'ton_client')],
+            `tonclient_${version}_${platform}${dstSuffix || ''}`, [__dirname, 'build']);
     }
 });
