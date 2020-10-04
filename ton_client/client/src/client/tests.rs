f7d45d00af30ae85d7627869d2768d57196ab0cd
Michael Vlasov <melsomino@gmail.com>
2020-10-04 23:42:32 +0500

    NEW: save api.json in api test

--- ton_client/client/src/client/tests.rs
+++ ton_client/client/src/client/tests.rs
@@ -1,5 +1,10 @@
 #[test]
 fn api_info() {
     let api = crate::client::get_api();
+    if let Some(mut path) = dirs::home_dir() {
+        path.push("api.json");
+        let json = serde_json::to_string(&api).unwrap();
+        std::fs::write(path, json);
+    }
     println!("{}", serde_json::to_string_pretty(&api).unwrap());
 }
