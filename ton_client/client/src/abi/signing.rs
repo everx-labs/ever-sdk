a2d43b076521d4bbc01b8bb2c4bfe21611aeb4f5
elasticLove1 <elasticLove@protonmail.ch>
2020-10-23 01:42:01 +0300

    Update signing.rs

--- ton_client/client/src/abi/signing.rs
+++ ton_client/client/src/abi/signing.rs
@@ -6,14 +6,15 @@ use crate::error::ClientResult;
 #[derive(Serialize, Deserialize, Clone, Debug, ApiType, PartialEq)]
 #[serde(tag="type")]
 pub enum Signer {
-    /// Message mustn't be signed.
+    /// No keys are provided. Creates an unsigned message. 
     None,
-    /// Message will be signed outside DApp.
-    /// Public key is provided in `hex` encoding.
+    /// Only public key is provided to generate unsigned message and `data_to_sign`
+    /// which can be signed later.  
     External { public_key: String },
-    /// Message will be signed using the provided keys.
+    /// Key pair is provided for signing
     Keys { keys: KeyPair },
-    /// Message will be signed using the provided Signing Box interface.
+    /// Signing Box interface is provided for signing, allows Dapps to sign messages using external APIs,
+    /// such as HSM, cold wallet, etc.
     SigningBox { handle: SigningBoxHandle },
 }
 
