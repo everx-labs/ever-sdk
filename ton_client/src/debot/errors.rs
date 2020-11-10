7f3a6cfe07436a2bcad71f15119623c9a5a6589f
Alexey Vavilin <mnogoborec@mail.ru>
2020-11-10 20:41:51 +0300

    Debot module embryo

---
+++ ton_client/src/debot/errors.rs
@@ -0,0 +1,28 @@
+/*
+* Copyright 2018-2020 TON DEV SOLUTIONS LTD.
+*
+* Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
+* this file except in compliance with the License.
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific TON DEV software governing permissions and
+* limitations under the License.
+*/
+
+use crate::error::ClientError;
+use std::fmt::Display;
+const DEBOT: isize = ClientError::DEBOT; // 200
+
+pub enum ErrorCode {
+    SampleCode = DEBOT + 1
+}
+pub struct Error;
+
+fn error(code: ErrorCode, message: String) -> ClientError {
+    ClientError::with_code_message(code as u32, message)
+}
+
+impl Error {
+}
