5f4c248bded1f89062bfac25ba7d0b06760f517d
elasticLove1 <elasticLove@protonmail.ch>
2020-12-10 14:46:07 +0300

    TVM docs

--- ton_client/src/tvm/run_message.rs
+++ ton_client/src/tvm/run_message.rs
@@ -170,7 +170,7 @@ pub struct ResultOfRunTvm {
     pub decoded: Option<DecodedOutput>,
 
     /// Updated account state BOC. Encoded as `base64`.
-    /// Attention! Only data in account state is updated.
+    /// Attention! only `account_state.storage.state.data` part of the boc is updated. 
     pub account: String,
 }
 
@@ -186,6 +186,28 @@ fn parse_transaction(
     )?
     .parsed)
 }
+
+/// Emulates all the phases of contract execution locally
+/// 
+/// Performs all the phases of contract execution on Transaction Executor - 
+/// the same component that is used on Validator Nodes. 
+/// 
+/// Can be used for contract debug, to find out the reason of message unsuccessful 
+/// delivery - as Validators just throw away failed transactions, here you can catch it. 
+/// 
+/// Another use case is to estimate fees for message execution. Set  `AccountForExecutor::Account.unlimited_balance`
+/// to `true` so that emulation will not depend on the actual balance.
+/// 
+/// One more use case - you can procude the sequence of operations,
+/// thus emulating the multiple contract calls locally. 
+/// And so on. 
+/// 
+/// To get the account boc (bag of cells) - use `net.query` method to download it from graphql api
+/// (field `boc` of `account`) or generate it with `abi.encode_account method`. 
+/// To get the message boc - use `abi.encode_message` or prepare it any other way, for instance, with Fift script.
+/// 
+/// If you need this emulation to be as precise as possible then specify `ParamsOfRunExecutor` parameter.
+/// If you need to see the aborted transaction as a result, not as an error, set `skip_transaction_check` to `true`.
 
 #[api_function]
 pub async fn run_executor(
@@ -254,6 +276,21 @@ pub async fn run_executor(
     })
 }
 
+
+/// Executes get methods of ABI-compatible contracts
+/// 
+/// Performs only a part of compute phase of transaction execution
+/// that is used to run get-methods of ABI-compatible contracts.
+///  
+/// If you try to run get methods with `run_executor` you will get an error, because it checks ACCEPT and exits
+/// if there is none, which is actually true for get methods. 
+///
+///  To get the account boc (bag of cells) - use `net.query` method to download it from graphql api
+/// (field `boc` of `account`) or generate it with `abi.encode_account method`. 
+/// To get the message boc - use `abi.encode_message` or prepare it any other way, for instance, with Fift script.
+/// 
+/// Attention! Updated account state is produces as well, but only 
+/// `account_state.storage.state.data`  part of the boc is updated. 
 #[api_function]
 pub async fn run_tvm(
     context: std::sync::Arc<ClientContext>,
