db896be59510bdee725fc110951b4577496a039c
elasticLove1 <elasticLove@protonmail.ch>
2020-12-24 15:51:44 +0300

    Config docs

--- ton_client/src/net/types.rs
+++ ton_client/src/net/types.rs
@@ -80,23 +80,50 @@ fn deserialize_reconnect_timeout<'de, D: Deserializer<'de>>(
 
 #[derive(Serialize, Deserialize, Debug, Clone, ApiType)]
 pub struct NetworkConfig {
+    /// DApp Server public address. 
+    /// For instance, for `net.ton.dev/graphql` GraphQL endpoint the server address will be net.ton.dev 
     pub server_address: Option<String>,
+
+    /// List of DApp Server addresses. Any correct URL format can be specified, including IP addresses
     pub endpoints: Option<Vec<String>>,
+
+    /// The number of automatic network retries that SDK performs in case of connection problems
+    /// The default value is 5.
     #[serde(default = "default_network_retries_count",
     deserialize_with = "deserialize_network_retries_count")]
     pub network_retries_count: i8,
+
+    /// The number of automatic message processing retries that SDK performs
+    /// in case of `Message Expired (507)` error - but only for those messages which 
+    /// local emulation was successfull or failed with replay protection error.
+    /// The default value is 5.
     #[serde(default = "default_message_retries_count",
     deserialize_with = "deserialize_message_retries_count")]
     pub message_retries_count: i8,
+
+    /// Timeout that is used to process message delivery for the contracts
+    /// which ABI does not include "expire" header.
+    /// If the message is not delivered within the speficied timeout 
+    /// the appropriate error occurs.
     #[serde(default = "default_message_processing_timeout",
     deserialize_with = "deserialize_message_processing_timeout")]
     pub message_processing_timeout: u32,
+
+    /// Maximum timeout that is used for query response.
+    /// The default value is 40 sec.
     #[serde(default = "default_wait_for_timeout",
     deserialize_with = "deserialize_wait_for_timeout")]
     pub wait_for_timeout: u32,
+
+    /// Maximum time difference between server and client. If client's device time is out of sink and difference is more than
+    /// the threshhold then error will occur. Also the error will occur if the specified threshhold is more than 
+    /// `message_processing_timeout/2`. 
+    /// The default value is 15 sec.
     #[serde(default = "default_out_of_sync_threshold",
     deserialize_with = "deserialize_out_of_sync_threshold")]
     pub out_of_sync_threshold: u32,
+
+    /// Timeout between reconnect attempts
     #[serde(default = "default_reconnect_timeout",
     deserialize_with = "deserialize_reconnect_timeout")]
     pub reconnect_timeout: u32,
