23ac152547b063108ae596436871cf6a9d4a32b6
Alexey Vavilin <mnogoborec@mail.ru>
2021-03-31 19:06:12 +0300

    query_counterparties

--- ton_client/src/net/mod.rs
+++ ton_client/src/net/mod.rs
@@ -14,9 +14,9 @@
 pub use batch::{batch_query, ParamsOfBatchQuery, ResultOfBatchQuery};
 pub use errors::{Error, ErrorCode};
 pub use queries::{
-    aggregate_collection, query, query_collection, wait_for_collection, ParamsOfQuery,
-    ParamsOfWaitForCollection, ResultOfAggregateCollection, ResultOfQuery, ResultOfQueryCollection,
-    ResultOfWaitForCollection,
+    aggregate_collection, query, query_collection, query_counterparties, wait_for_collection,
+    ParamsOfQuery, ParamsOfWaitForCollection, ResultOfAggregateCollection, ResultOfQuery, 
+    ResultOfQueryCollection, ResultOfWaitForCollection,
 };
 pub(crate) use server_link::{ServerLink, MAX_TIMEOUT};
 pub use subscriptions::{
@@ -25,8 +25,8 @@ pub use subscriptions::{
 };
 pub use ton_gql::{
     AggregationFn, FieldAggregation, GraphQLOperationEvent, OrderBy,
-    ParamsOfAggregateCollection, ParamsOfQueryCollection, ParamsOfQueryOperation, PostRequest,
-    SortDirection,
+    ParamsOfAggregateCollection, ParamsOfQueryCollection, ParamsOfQueryCounterparties,
+    ParamsOfQueryOperation, PostRequest, SortDirection,
 };
 pub use types::{
     NetworkConfig, BLOCKS_TABLE_NAME, CONTRACTS_TABLE_NAME, MESSAGES_TABLE_NAME,
