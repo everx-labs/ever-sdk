b3bfe4ca107ce7709e1436c48d831b5b1ed3f08d
Denis K <@tonlabs.io>
2021-07-30 08:50:35 +0300

    Added documentation for Rust API.

---
+++ docs/rust-api/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ton_client"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"ton_client/tvm/enum.StdContractError.html\" title=\"enum ton_client::tvm::StdContractError\">StdContractError</a>","synthetic":false,"types":["ton_client::tvm::errors::StdContractError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/rust-api/implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["ton_client"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"ton_client/error/struct.ClientError.html\" title=\"struct ton_client::error::ClientError\">ClientError</a>","synthetic":false,"types":["ton_client::error::ClientError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
