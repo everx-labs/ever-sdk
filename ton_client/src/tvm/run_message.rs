b4f89042fd35c2066e3eef9ab0aff445bfda4a35
Alexey Vavilin <mnogoborec@mail.ru>
2021-08-18 18:28:06 +0300

    Default config from mainnet

--- ton_client/src/tvm/run_message.rs
+++ ton_client/src/tvm/run_message.rs
@@ -231,7 +231,7 @@ async fn parse_transaction(
 /// 
 /// If you need this emulation to be as precise as possible (for instance - emulate transaction
 /// with particular lt in particular block or use particular blockchain config, 
-/// in case you want to download it from a particular key block - then specify `ParamsOfRunExecutor` parameter.
+/// downloaded from a particular key block - then specify `execution_options` parameter.
 /// 
 /// If you need to see the aborted transaction as a result, not as an error, set `skip_transaction_check` to `true`.
 
