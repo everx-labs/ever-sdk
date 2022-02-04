63402219dcd68eb0341e07e63567f304926f5c03
Michael Vlasov <melsomino@gmail.com>
2022-02-04 17:05:45 +0300

    NEW: support for MYCODE tvm op in tvm_run and tvm_run_get

--- ton_client/src/tvm/run_message.rs
+++ ton_client/src/tvm/run_message.rs
@@ -183,7 +183,7 @@ pub struct ResultOfRunTvm {
     pub decoded: Option<DecodedOutput>,
 
     /// Updated account state BOC. Encoded as `base64`.
-    /// Attention! Only `account_state.storage.state.data` part of the BOC is updated. 
+    /// Attention! Only `account_state.storage.state.data` part of the BOC is updated.
     pub account: String,
 }
 
@@ -202,37 +202,37 @@ async fn parse_transaction(
 }
 
 /// Emulates all the phases of contract execution locally
-/// 
-/// Performs all the phases of contract execution on Transaction Executor - 
-/// the same component that is used on Validator Nodes. 
-/// 
+///
+/// Performs all the phases of contract execution on Transaction Executor -
+/// the same component that is used on Validator Nodes.
+///
 /// Can be used for contract debugging, to find out the reason why a message was not delivered successfully.
-/// Validators throw away the failed external inbound messages (if they failed bedore `ACCEPT`) in the real network. 
-/// This is why these messages are impossible to debug in the real network. 
+/// Validators throw away the failed external inbound messages (if they failed bedore `ACCEPT`) in the real network.
+/// This is why these messages are impossible to debug in the real network.
 /// With the help of run_executor you can do that. In fact, `process_message` function
 /// performs local check with `run_executor` if there was no transaction as a result of processing
-/// and returns the error, if there is one. 
-/// 
-/// Another use case to use `run_executor` is to estimate fees for message execution. 
+/// and returns the error, if there is one.
+///
+/// Another use case to use `run_executor` is to estimate fees for message execution.
 /// Set  `AccountForExecutor::Account.unlimited_balance`
 /// to `true` so that emulation will not depend on the actual balance.
 /// This may be needed to calculate deploy fees for an account that does not exist yet.
 /// JSON with fees is in `fees` field of the result.
-/// 
+///
 /// One more use case - you can produce the sequence of operations,
-/// thus emulating the sequential contract calls locally. 
-/// And so on. 
-/// 
-/// Transaction executor requires account BOC (bag of cells) as a parameter. 
+/// thus emulating the sequential contract calls locally.
+/// And so on.
+///
+/// Transaction executor requires account BOC (bag of cells) as a parameter.
 /// To get the account BOC - use `net.query` method to download it from GraphQL API
 /// (field `boc` of `account`) or generate it with `abi.encode_account` method.
-///  
+///
 /// Also it requires message BOC. To get the message BOC - use `abi.encode_message` or `abi.encode_internal_message`.
-/// 
+///
 /// If you need this emulation to be as precise as possible (for instance - emulate transaction
-/// with particular lt in particular block or use particular blockchain config, 
+/// with particular lt in particular block or use particular blockchain config,
 /// downloaded from a particular key block - then specify `execution_options` parameter.
-/// 
+///
 /// If you need to see the aborted transaction as a result, not as an error, set `skip_transaction_check` to `true`.
 
 #[api_function]
@@ -321,19 +321,19 @@ pub async fn run_executor_internal(
 
 
 /// Executes get-methods of ABI-compatible contracts
-/// 
+///
 /// Performs only a part of compute phase of transaction execution
 /// that is used to run get-methods of ABI-compatible contracts.
-///  
+///
 /// If you try to run get-methods with `run_executor` you will get an error, because it checks ACCEPT and exits
-/// if there is none, which is actually true for get-methods. 
+/// if there is none, which is actually true for get-methods.
 ///
 ///  To get the account BOC (bag of cells) - use `net.query` method to download it from GraphQL API
-/// (field `boc` of `account`) or generate it with `abi.encode_account method`. 
+/// (field `boc` of `account`) or generate it with `abi.encode_account method`.
 /// To get the message BOC - use `abi.encode_message` or prepare it any other way, for instance, with FIFT script.
-/// 
-/// Attention! Updated account state is produces as well, but only 
-/// `account_state.storage.state.data`  part of the BOC is updated. 
+///
+/// Attention! Updated account state is produces as well, but only
+/// `account_state.storage.state.data`  part of the BOC is updated.
 #[api_function]
 pub async fn run_tvm(
     context: std::sync::Arc<ClientContext>,
@@ -343,7 +343,7 @@ pub async fn run_tvm(
     let message = deserialize_object_from_boc::<Message>(&context, &params.message, "message").await?.object;
     let options = ResolvedExecutionOptions::from_options(&context, params.execution_options).await?;
     if account.object.is_none() {
-        return Err(Error::invalid_account_boc("Acount is None"))
+        return Err(Error::invalid_account_boc("Account is None"))
     }
 
     let messages = super::call_tvm::call_tvm_msg(&mut account.object, options, &message)?;
