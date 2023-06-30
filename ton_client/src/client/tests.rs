2ff5d28250561cbf19aadcef41440578e64dba4b
Michael Vlasov <melsomino@gmail.com>
2023-06-30 16:52:28 +0300

    FIX: removed duplicated async to sync implementation

--- ton_client/src/client/tests.rs
+++ ton_client/src/client/tests.rs
@@ -82,11 +82,6 @@ fn test_invalid_params_error_secret_stripped() {
     assert!(!error.message.contains(secret));
 }
 
-#[test]
-fn test_sync_calls() {
-
-}
-
 #[tokio::test]
 async fn test_memory_leak() {
     for _ in 0..1 {
